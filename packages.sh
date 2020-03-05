1e50dbafcc9295c31aae5ac2eaca6d6a527113fe
Alex Long <lngtth@longtooth.cloud>
2020-03-05 11:10:39 -0800

    Add packages script

---
+++ packages.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+utils='
+htop
+colorls
+'
+
+programming='
+python
+go
+' 
+
+applications='
+rxvt-unicode
+neovim
+mpd
+ncmpcpp
+mpv
+ranger
+feh
+calcurse
+sc
+neomutt
+lynx
+surf
+transmission
+dmenu
+gimp
+zathura
+zathura-cb
+zathura-djvu
+zathura-pdf-mupdf
+'
+
+input='
+ibus-anthy
+ja-fonts-gnu
+ja-sazanami-ttf
+noto-cjk
+'
+
+fonts='
+comic-neue
+font-awesome
+inconsolata-font
+'
+#TODO Twemoji
+#TODO CC Wild Words
+
+doas pkg_add $utils $programming $applications $input $fonts 
+
+# Choose a package when the base name is ambiguous
+#TODO automate this too
+# python -> python-3.X.X
+# neomutt -> neomutt-XXXXXXXXpX-gpgme
